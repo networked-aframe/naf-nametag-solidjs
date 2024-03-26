01ef6b9ea069f822d342cd71f5f181ef58c7cbf9
Vincent Fretin <vincent.fretin@gmail.com>
2024-03-26 14:17:28 +0100

    use NAF.utils.vectorRequiresUpdate in the head-template schema

--- src/player-info.js
+++ src/player-info.js
@@ -6,7 +6,17 @@ NAF.schemas.getComponents = (template) => {
   if (!NAF.schemas.hasTemplate('#head-template')) {
     NAF.schemas.add({
       template: '#head-template',
-      components: ['position', 'rotation', 'player-info'],
+      components: [
+        {
+          component: 'position',
+          requiresNetworkUpdate: NAF.utils.vectorRequiresUpdate(0.001),
+        },
+        {
+          component: 'rotation',
+          requiresNetworkUpdate: NAF.utils.vectorRequiresUpdate(0.5),
+        },
+        'player-info',
+      ],
     });
   }
 
